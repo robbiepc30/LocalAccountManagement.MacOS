d9f7b12826196a1c302accf09ca5068722888dd3
robbiepc30 <robbiepc30@users.noreply.github.com>
2016-08-19 15:36:13 -0800

    formating

--- Get-LocalUser.ps1
+++ Get-LocalUser.ps1
@@ -3,7 +3,7 @@ function Get-LocalUser {
 
     foreach ($up in $userPlist) {
         $userHashTable = @{}
-        $up.key | ForEach-Object -Begin {$i = 0} -Process { $userHashTable.Add($_.Split(':')[1], $up.array[$i].string); $i++}
+        $up.key | ForEach-Object -Begin { $i = 0 } -Process { $userHashTable.Add($_.Split(':')[1], $up.array[$i].string); $i++ }
 
         [PSCustomObject] @{ Name = if ($userHashTable.RecordName.count -gt 1) { $userHashTable.RecordName[0]} else { $userHashTable.RecordName }
                             DisplayName = $userHashTable.RealName
